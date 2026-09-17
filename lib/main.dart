bce2ef78222126f4d4517a7eaaec6607cf85565f
Paawan Pawar <paawan-pawar@gmail.com>
2026-09-17 22:24:10 +0530

    sh

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Kavach',
+      title: 'Sahasraksha',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
